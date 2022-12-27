79405161953bd89f912099b8d16f4659f6077299
Ramya <Ramya@LAPTOP-M4C42PBQ>
2022-12-27 08:34:53 +0530

    Script added

--- src/main/java/com/module/scripts/Login.java
+++ src/main/java/com/module/scripts/Login.java
@@ -1,5 +1,8 @@
 package com.module.scripts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import com.hcl.base.BaseClass;
@@ -8,7 +11,24 @@ public class Login extends BaseClass {
 	
 	@Test
 	public void sampel() {
-		System.out.println("sjdfgjdsfdsfd");
+		
+
+		
+		//Actions actions=new Actions(driver);
+		Actions actions=new Actions(driver);
+		actions.sendKeys(driver.findElement(By.id("email")), "ramyashihb").build().perform();
+
+		actions.sendKeys(driver.findElement(By.id("pass")), "1234456").build().perform();
+
+		//actions.click(driver.findElement(By.name("login"))).build().perform();
+
+		
+		//driver.findElement(By.name("login")).submit();
+		//driver.findElement(By.name("login")).click();
+
+		//actions.sendKeys(Keys.ENTER).build().perform();
+		actions.sendKeys(Keys.ENTER).build().perform();
+		
 	}
 
 }
