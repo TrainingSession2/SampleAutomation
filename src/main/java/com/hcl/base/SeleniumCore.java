53d38489624ed045f8a0cc168279e027806f0c9c
Gayathri <DELL LATITUDE 7480@DESKTOP-J7IE0DD>
2022-12-29 08:33:32 +0530

    Updated Code

--- src/main/java/com/hcl/base/SeleniumCore.java
+++ src/main/java/com/hcl/base/SeleniumCore.java
@@ -1,5 +1,16 @@
 package com.hcl.base;
 
-public class SeleniumCore {
+import org.testng.annotations.Test;
+
+public class SeleniumCore extends SeleniumUtils {
+	
+	@Test
+	public void sample() {
+		enterTextOnElement("id", "email", "Gayathri");
+		enterTextOnElement("id", "pass", "123fsfjhv");
+		waitForSec(4000);
+		clickOnElement("name", "login");
+		waitForSec(4000);
+	}
 
 }
