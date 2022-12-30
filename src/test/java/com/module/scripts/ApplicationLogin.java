28a20513bc2c6e67834f1e387552263d585291e6
Gopikrishna <Admin@DESKTOP-RTCHCLV>
2022-12-30 08:58:01 +0530

    Added login script

---
+++ src/test/java/com/module/scripts/ApplicationLogin.java
@@ -0,0 +1,19 @@
+package com.module.scripts;
+
+import org.testng.annotations.Test;
+
+import com.hcl.base.BaseClass;
+import com.hcl.base.SeleniumUtils;
+
+public class ApplicationLogin extends BaseClass {
+
+	SeleniumUtils seleniumUtils=new SeleniumUtils();
+
+	@Test
+	public void loginApplication() {
+		seleniumUtils.enterTextOnElement("name", "username", "Admin");
+		seleniumUtils.enterTextOnElement("name", "password", "admin123");
+		seleniumUtils.clickOnElement("xpath", "//button[@type='submit']");
+	}
+
+}
