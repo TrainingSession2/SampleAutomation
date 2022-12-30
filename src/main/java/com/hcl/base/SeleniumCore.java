12f45509104f6b735106752d79ddfd8e9f6ff335
Naga Lakshmi <Admin@DESKTOP-8S9CVUK>
2022-12-30 08:37:55 +0530

    Added new methods

--- src/main/java/com/hcl/base/SeleniumCore.java
+++ src/main/java/com/hcl/base/SeleniumCore.java
@@ -1,14 +1,31 @@
 package com.hcl.base;
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class SeleniumCore extends BaseClass{
 
+	public static Actions actions;
+	
 	public void pressEnterButton() {
-
-		Actions actions =new Actions(driver);
+		actions =new Actions(driver);
 		actions.sendKeys(Keys.ENTER).build().perform();
+	}
+	
+	public void pressTabButton() {
+		actions =new Actions(driver);
+		actions.sendKeys(Keys.TAB).build().perform();
+	}
+	
+	public void pressEscButton() {
+		actions =new Actions(driver);
+		actions.sendKeys(Keys.ESCAPE).build().perform();
+	}
+	
+	public void clickOnMenu(WebElement element) {
+		actions =new Actions(driver);
+		actions.click(element).build().perform();
 	}
 
 }
