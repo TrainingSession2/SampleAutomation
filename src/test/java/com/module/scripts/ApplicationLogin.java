81c47d3721a12e59d712782af8ddf1348909cc60
Gopikrishna <Admin@DESKTOP-RTCHCLV>
2023-01-05 08:57:57 +0530

    Updated login utils

--- src/test/java/com/module/scripts/ApplicationLogin.java
+++ src/test/java/com/module/scripts/ApplicationLogin.java
@@ -3,18 +3,15 @@ package com.module.scripts;
 import org.testng.annotations.Test;
 
 import com.hcl.base.BaseClass;
-import com.hcl.base.SeleniumUtils;
+import com.module.utils.LoginUtils;
 
 public class ApplicationLogin extends BaseClass {
 
-	SeleniumUtils seleniumUtils=new SeleniumUtils();
+	LoginUtils loginUtils=new LoginUtils();
 
 	@Test
 	public void loginApplication() {
-		seleniumUtils.waitForSec(3000);
-		seleniumUtils.enterTextOnElement("name", "username", "Admin");
-		seleniumUtils.enterTextOnElement("name", "password", "admin123");
-		seleniumUtils.clickOnElement("xpath", "//button[@type='submit']");
+		loginUtils.loginCredentilas(pro.getProperty("username"),pro.getProperty("password"));
 	}
 
 }
